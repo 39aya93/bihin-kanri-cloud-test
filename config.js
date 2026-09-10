56c1881abd7a45a8e958a4b058708613a6e7959c
39aya93 <39aya93@gmail.com>
2026-09-11 08:21:18 +0900

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
  *    手動で編集する必要は基本ありません。
  */
 const APP_CONFIG = {
-  MANAGER_URL: 'https://script.google.com/macros/s/AKfycby7DbPc2qa0UrFgLL0nvH1mD7nNec6vrn9z74Xzj1fGDEh3POZ93UZoS8seAzXYJWU9/exec',
+  MANAGER_URL: 'https://script.google.com/macros/s/AKfycbxJ4n9j2G2rAUCo3hWtWrkFkc0BKR8yHA4ljHOvyj2t9iWmF5bNsRa2raAkT9_qfmEm/exe',
   DB_LIST: [],
   ACTIVE_DB_ID: ''
 };
