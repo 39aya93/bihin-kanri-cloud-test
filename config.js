c3c2c67743564a97b81d7ff280b722662ae9e486
39aya93 <39aya93@gmail.com>
2026-09-11 11:36:00 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
-/*
+/**
  * 備品管理アプリの接続設定
  *
  * 1) MANAGER_URL に「DB管理用Apps Script」のWebアプリURLを入れます。
  * 2) DB_LIST はDB作成後に「DB一覧を更新」ボタンで自動更新できます。
  *    手動で編集する必要は基本ありません。
  */
-const APP_CONFIG = {
-  MANAGER_URL: 'https://script.google.com/macros/s/AKfycbxHhhSHwYUJ4-cwSnn7l9jmi8Kxa14az-koZR2NApwx9E-fTDVz0M4ldBts8B1lIIAw/exec',
-  DB_LIST: [],
-  ACTIVE_DB_ID: ''
-};
+
+const MANAGER_URL = 'https://script.google.com/macros/s/AKfycbxHhhSHwYUJ4-cwSnn7l9jmi8Kxa14az-koZR2NApwx9E-fTDVz0M4ldBts8B1lIIAw/exec';
