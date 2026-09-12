cf17686992b3eda6a50a4de479f206b896e0f03c
39aya93 <39aya93@gmail.com>
2026-09-12 16:16:25 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
  * 備品管理アプリの接続設定
  */
 const CONFIG = {
-  MANAGER_URL: 'https://script.google.com/macros/s/AKfycbw8WuoE88uKpOvQH6NI0F_nzvuLRpvkSwwNzNot3YKbHZKT7YE5sbKgqK7SBNWgbBQ/exec'
+  MANAGER_URL: 'https://script.google.com/macros/s/AKfycbwsYbZYbyydw4RsW2xzDCbsmmqDqLPsU-IMg54X5bOKKVE2pHgoZZw4I19jLnb-H_2A/exec'
 };
